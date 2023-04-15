f76cb28ac8000b732a86582a99468b2db52e362a
Oleh <my.mail.olech@gmail.com>
2023-04-15 22:53:21 +0300

    added navigation menu component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="flex items-center text-blue-dark text-base font-poppins font-normal">
+    <ul class="flex items-center gap-9">
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/about-us">About Us</router-link>
+      </li>
+      <li>
+        <router-link to="/destinations">Destinations</router-link>
+      </li>
+      <li>
+        <router-link to="/tours">Tours</router-link>
+      </li>
+      <li>
+        <router-link to="/blog">Blog</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped></style>
