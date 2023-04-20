735572c3755258f96d6b81786b89acd618b09f53
Oleh <my.mail.olech@gmail.com>
2023-04-20 14:28:52 +0300

    refactor, added styles for active link

--- src/components/LocationCard.vue
+++ src/components/LocationCard.vue
@@ -1,14 +1,14 @@
 <template>
-  <div :class="['location-card', props.class]">
+  <div :class="['relative', props.class]">
     <div
-      class="location-card__rating px-[10px] py-[5px]
+      class="absolute top-6 left-6 px-10 py-[5px]
          text-gray-dark text-sm leading-none font-medium bg-white rounded-xl"
     >
       {{ props.data.score }}
     </div>
-    <div class="location-card__location text-white">
-      <div class="location-card__location__country text-xl  font-semibold leading-none">{{ props.data.country }}</div>
-      <div class="location-card__location__location text-sm leading-none">{{ props.data.location }}</div>
+    <div class="absolute bottom-6 left-6 text-white">
+      <div class="mb-10 text-xl  font-semibold leading-none">{{ props.data.country }}</div>
+      <div class="text-sm leading-none">{{ props.data.location }}</div>
     </div>
     <img :src="props.data.imgUrl" alt="">
   </div>
@@ -27,28 +27,5 @@ const props = defineProps<IProps>()
 </script>
 
 <style lang="scss">
-.location-card {
-    position: relative;
 
-    &__rating {
-      top: 24px;
-      left: 24px;
-      position: absolute;
-    }
-
-    &__location {
-      position: absolute;
-      left: 24px;
-      bottom: 24px;
-
-        &__country {
-          margin-bottom: 10px;
-
-        }
-
-        &__location {
-
-        }
-    }
-  }
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,24 +2,41 @@
   <nav class="flex items-center text-blue-dark text-base font-poppins font-normal">
     <ul class="flex items-center gap-9">
       <li>
-        <router-link to="/">Home</router-link>
+        <router-link :class="['link', {'active': currentRouteName === '/'} ]" to="/">Home</router-link>
       </li>
       <li>
-        <router-link to="/about-us">About Us</router-link>
+        <router-link class="link" to="/about-us">About Us</router-link>
       </li>
       <li>
-        <router-link to="/destinations">Destinations</router-link>
+        <router-link class="link" to="/destinations">Destinations</router-link>
       </li>
       <li>
-        <router-link to="/tours">Tours</router-link>
+        <router-link class="link" to="/tours">Tours</router-link>
       </li>
       <li>
-        <router-link to="/blog">Blog</router-link>
+        <router-link class="link" to="/blog">Blog</router-link>
       </li>
     </ul>
   </nav>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const router = useRouter()
+const currentRouteName = computed(() => router.currentRoute.value.name)
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.router-link-exact-active {
+  position: relative;
+
+  &::after {
+      position: absolute;
+      content: "";
+      bottom: -6px;
+      left: 0;
+      width: 12px;
+      height: 2px;
+      background-color: #f66f4d;
+  }
+}
+</style>
