8f0f8b39844ed85a7ce53275d6bcb7aa2c4b1065
Oleh <my.mail.olech@gmail.com>
2023-05-04 01:02:31 +0300

    deleted unused components, fixed table form validation

--- src/views/contacts/components/tab-views/table-view/TableView.vue
+++ src/views/contacts/components/tab-views/table-view/TableView.vue
@@ -1,83 +1,84 @@
 <template>
-  <el-table :data="contacts" @row-click="onItemClick">
-    <el-table-column
-      width="130"
-      prop="image"
-      label="Contact Photo"
-    >
-      <template #default="scope">
-        <AppAvatarImage
-          :src="scope.row.image"
-          :name="scope.row.name"
-          class="flex w-32 h-32 items-center justify-center bg-gray-ultra-light"
-        />
-      </template>
-    </el-table-column>
+  <el-form id="tableViewEditForm" ref="formRef" :rules="formRules" :model="localContact">
+    <el-table :data="contacts" @row-click="onItemClick">
+      <el-table-column
+        width="130"
+        prop="image"
+        label="Contact Photo"
+      >
+        <template #default="scope">
+          <AppAvatarImage
+            :src="scope.row.image"
+            :name="scope.row.name"
+            class="flex w-32 h-32 items-center justify-center bg-gray-ultra-light"
+          />
+        </template>
+      </el-table-column>
 
-    <el-table-column
-      width="200"
-      prop="name"
-      label="Fullname"
-    >
-      <template #default="scope">
-        <el-form-item v-if="editMode === scope.row.id" class="no-margin" prop="name" @click.stop>
-          <el-input v-model="localContact.name" form="tableViewEditForm" />
-        </el-form-item>
-        <template v-else>{{ scope.row.name }}</template>
-      </template>
-    </el-table-column>
-    <el-table-column
-      width="800"
-      prop="description"
-      label="Description"
-    >
-      <template #default="scope">
-        <el-form-item v-if="editMode === scope.row.id" class="no-margin" prop="description" @click.stop>
-          <el-input v-model="localContact.description" form="tableViewEditForm" />
-        </el-form-item>
-        <template v-else>{{ scope.row.description }}</template>
-      </template>
-    </el-table-column>
-    <el-table-column
-      class="flex items-center"
-      label="Actions"
-      fixed="right"
-      width="150"
-    >
-      <template #default="scope">
-        <div class="flex">
-          <template v-if="editMode === scope.row.id">
-            <el-button
-              @click.stop="onCancel"
-            >
-              Cancel
-            </el-button>
-            <el-button
-              :type="$elComponentType.primary"
-              @click.stop="onItemSave(localContact)"
-            >
-              Save
-            </el-button>
-          </template>
-          <template v-else>
-            <el-button
-              :type="$elComponentType.danger"
-              @click.stop="onItemDelete(scope.row)"
-            >
-              Delete
-            </el-button>
-            <el-button
-              :type="$elComponentType.primary"
-              @click.stop="onEdit(scope.row)"
-            >
-              Edit
-            </el-button>
-          </template>
-        </div>
-      </template>
-    </el-table-column>
-  </el-table>
-  <el-form id="tableViewEditForm" ref="formRef" :rules="formRules" :model="localContact" />
+      <el-table-column
+        width="200"
+        prop="name"
+        label="Fullname"
+      >
+        <template #default="scope">
+          <el-form-item v-if="editMode === scope.row.id" class="no-margin" prop="name" @click.stop>
+            <el-input v-model="localContact.name" />
+          </el-form-item>
+          <template v-else>{{ scope.row.name }}</template>
+        </template>
+      </el-table-column>
+      <el-table-column
+        width="800"
+        prop="description"
+        label="Description"
+      >
+        <template #default="scope">
+          <el-form-item v-if="editMode === scope.row.id" class="no-margin" prop="description" @click.stop>
+            <el-input v-model="localContact.description" />
+          </el-form-item>
+          <template v-else>{{ scope.row.description }}</template>
+        </template>
+      </el-table-column>
+      <el-table-column
+        class="flex items-center"
+        label="Actions"
+        fixed="right"
+        width="150"
+      >
+        <template #default="scope">
+          <div class="flex">
+            <template v-if="editMode === scope.row.id">
+              <el-button
+                @click.stop="onCancel"
+              >
+                Cancel
+              </el-button>
+              <el-button
+                :type="$elComponentType.primary"
+                @click.stop="onItemSave(localContact)"
+              >
+                Save
+              </el-button>
+            </template>
+            <template v-else>
+              <el-button
+                :type="$elComponentType.danger"
+                @click.stop="onItemDelete(scope.row)"
+              >
+                Delete
+              </el-button>
+              <el-button
+                :type="$elComponentType.primary"
+                @click.stop="onEdit(scope.row)"
+              >
+                Edit
+              </el-button>
+            </template>
+          </div>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-form>
 </template>
 
 <script lang="ts" setup>
